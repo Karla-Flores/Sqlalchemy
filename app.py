fa4ddb9c98415813753cfb4dea059cc05a1e408a
Karla-Flores <ms.kaflores@gmail.com>
2021-06-25 23:13:40 -0500

    Update app.py
    precipitation

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect
 from flask import Flask, jsonify
-
+import datetime as dt
+from datetime import datetime
+from datetime import date, timedelta
 
 #################################################
 # Database Setup
@@ -31,15 +33,18 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     """List all available api routes."""
-    return (f'<table><thead><tr><th>/api/v1.0/precipitation<br></th><th></th><th><a href="http://127.0.0.1:5000/api/v1.0/precipitation">Precipitation</a><br></th></table>'
+    return (f'<table><thead><tr><th>/api/v1.0/precipitation</th><th>      </th><th><a href="http://127.0.0.1:5000/api/v1.0/precipitation">Precipitation</a><br></th></table>'
             f'<table><thead><tr><th>/api/v1.0/stations<br></th><th></th><th><a href="http://127.0.0.1:5000/api/v1.0/stations">Stations</a><br></th></table>'
-            f'<table><thead><tr><th>/api/v1.0/tobs<br></th><th></th><th><a href="http://127.0.0.1:5000/api/v1.0/tobs">Stations</a><br></th></table>'
+            f'<table><thead><tr><th>/api/v1.0/tobs<br></th><th></th><th><a href="http://127.0.0.1:5000/api/v1.0/tobs">Temperature Observations</a><br></th></table>'
             f'<table><thead><tr><th>/api/v1.0/<start><br></th><th></th><th><a href="http://127.0.0.1:5000/api/v1.0/<start>">Start</a><br></th></table>'
             f'<table><thead><tr><th>/api/v1.0/<start>/<end><br></th><th></th><th><a href="http://127.0.0.1:5000/api/v1.0/<start>/<end>">Start and End</a><br></th></table>')
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     session = Session(engine)
+    max_d = session.query(measurement.date).order_by(measurement.date.desc()).first()[0]
+    max_d = datetime.strptime(max_d, '%Y-%m-%d')
+    last_year = max_d - timedelta(days=366)
     oy_precipitation = session.query(measurement.date,measurement.prcp).filter(measurement.date >= last_year).order_by(measurement.date).all()
     session.close()
    
