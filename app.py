c9b0bc734be98ab4ec8b5ef46b21a93851244bc4
Karla-Flores <ms.kaflores@gmail.com>
2021-06-25 23:29:35 -0500

    Stations

--- app.py
+++ app.py
@@ -30,24 +30,29 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+# Home
 @app.route("/")
 def home():
     """List all available api routes."""
-    return (f'<table><thead><tr><th>/api/v1.0/precipitation</th><th>      </th><th><a href="http://127.0.0.1:5000/api/v1.0/precipitation">Precipitation</a><br></th></table>'
+    return (f'<table><thead><tr><th>/api/v1.0/precipitation</th><th></th><th><a href="http://127.0.0.1:5000/api/v1.0/precipitation">Precipitation</a><br></th></  table>'
             f'<table><thead><tr><th>/api/v1.0/stations<br></th><th></th><th><a href="http://127.0.0.1:5000/api/v1.0/stations">Stations</a><br></th></table>'
             f'<table><thead><tr><th>/api/v1.0/tobs<br></th><th></th><th><a href="http://127.0.0.1:5000/api/v1.0/tobs">Temperature Observations</a><br></th></table>'
             f'<table><thead><tr><th>/api/v1.0/<start><br></th><th></th><th><a href="http://127.0.0.1:5000/api/v1.0/<start>">Start</a><br></th></table>'
             f'<table><thead><tr><th>/api/v1.0/<start>/<end><br></th><th></th><th><a href="http://127.0.0.1:5000/api/v1.0/<start>/<end>">Start and End</a><br></th></table>')
 
+
+# Precipitation
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    # Create session from Python to DB
     session = Session(engine)
+    # Defining Last year
     max_d = session.query(measurement.date).order_by(measurement.date.desc()).first()[0]
     max_d = datetime.strptime(max_d, '%Y-%m-%d')
     last_year = max_d - timedelta(days=366)
+    # Query the last 12 months precipitation
     oy_precipitation = session.query(measurement.date,measurement.prcp).filter(measurement.date >= last_year).order_by(measurement.date).all()
     session.close()
-   
     # Convert list of tuples into normal list
     precip = []
     for date, prcp in oy_precipitation:
@@ -57,6 +62,19 @@ def precipitation():
         precip.append(p_dict)
 
     return jsonify(precip)
+
+# Stations
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create session from Python to DB
+    session = Session(engine)
+    # Query list of stations
+    list_stations=session.query(station.station).all()
+    session.close()
+    # Convert list of tuples into normal list
+    stations = list(np.ravel(list_stations))
+
+    return jsonify(stations)
 
 if __name__ == '__main__':
     app.run(debug=True)
