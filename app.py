5eebec0a384a60ee0b91ffa8bf29d220253aed89
Karla-Flores <ms.kaflores@gmail.com>
2021-06-25 21:19:13 -0500

    Review an AttributeError

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import numpy as np
-
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect
-
 from flask import Flask, jsonify
 
 
@@ -12,16 +10,13 @@ from flask import Flask, jsonify
 # Database Setup
 #################################################
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
-
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
-
 # Save reference to the table
 measurement = Base.classes.measurement
 station = Base.classes.station
-
 # Create session from Python to DB
 session = Session(engine)
 
@@ -29,3 +24,24 @@ session = Session(engine)
 # Flask Setup
 #################################################
 app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
+@app.route("/")
+def home():
+    """List all available api routes."""
+    return (f'/api/v1.0/precipitation<br>'
+            f'/api/v1.0/stations<br>'
+            f'/api/v1.0/tobs<br>'
+            f'/api/v1.0/<start><br>'
+            f'/api/v1.0/<start>/<end><br>')
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+    session.close()
+
+if __name__ == '__main__':
+    app.run(debug=True)
